d8f0812e0719f48d7ba11881b84147e0d71a2dfa
Mohamad Dwikuntobayu <dwikunto.bayu@kiranatama.com>
2016-02-03 17:02:39 +0700

    add seed data \n add message to api

--- app/api/v1/bycyclev1.rb
+++ app/api/v1/bycyclev1.rb
@@ -1,76 +1,63 @@
 module API
 
   class BycycleV1 < Grape::API
-
     prefix "v1"
-
     format :json
-
-    
 
     http_basic do |username, password|
       #api grape only know base64 encode
       #Base64.urlsafe_encode64(username)
       user = User.where(username: username).first
-
       user.authorized? password unless user.blank?
-
     end
 
     resource :bycycles do
 
       desc "Get all bycycles"
-
       get '/' do
-
         @bycycles = Bycycle.all
-
         {bycycles: @bycycles, total: @bycycles.count, status: 200}
-
       end
 
       desc "Add new bycycle"
-
       post '/' do
-
-        Bycycle.create!({name: params[:name], series: params[:series]})
-
+        if Bycycle.create!({name: params[:name], series: params[:series]})
+          {status: 200, message: "Success add bycyle"}
+        else
+          {status: 400, message: "Fails add bycycle"}
+        end
       end
-
-      
 
       desc "Show a bycycle"
-
       get '/:id' do
-
         @bycycle = Bycycle.find_by_id params[:id]
-
-        {bycycles: @bycycle, status: 200}
-
+        if @bycycle.present?
+          {bycycles: @bycycle, message: "Found", status: 200}
+        else
+          {bycycles: @bycycle, message: "Empty", status: 400}
+        end
       end
-
-      
 
       desc "Update a bycycle"
-
       put '/:id' do
-
-  Bycycle.find_by_id(params[:id]).update({name: params[:name], series: params[:series]})
-
+      @bycycle = Bycycle.find_by_id(params[:id])
+      if @bycycle.update({name: params[:name], series: params[:series]})
+        {status: 200, message: "Success update bycycle"}
+      else
+        {status: 400, message: "Fails update bycycle"}
+      end
       end
 
-      
-
       desc "Delete a data"
-
       delete '/:id' do
-
-        Bycycle.find_by_id(params[:id]).destroy
-
+        @bycycle = Bycycle.find_by_id(params[:id])
+        if @bycycle.destroy
+          {status: 200, message: "Success delete bycycle"}
+        else
+          {status: 400, message: "Fails delete bycycle"}
+        end
       end
 
     end
-
   end
-
 end
